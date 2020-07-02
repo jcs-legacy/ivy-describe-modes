0c4b9324e4361ac73c6c7d36deb93e8ba173680d
jenchieh <jcs090218@gmail.com>
2020-07-02 19:56:18 +0800

    First add file.

---
+++ ivy-describe-modes.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: ivy-describe-modes.el $
+;; $Date: 2020-07-02 19:56:01 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2020 by Shen, Jen-Chieh $
+;; ========================================================================
