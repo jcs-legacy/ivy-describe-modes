c3dd859552a26eac3b7e7efe26d51308b9a0d09b
jenchieh <jcs090218@gmail.com>
2020-07-02 21:46:02 +0800

    Complete first version.

--- ivy-describe-modes.el
+++ ivy-describe-modes.el
@@ -41,8 +41,14 @@
 (defconst ivy-describe-modes--prompt "[Describe Modes]: "
   "Prompt string when using `ivy-describe-modes'.")
 
+(defconst ivy-describe-modes--apropos-name "*Apropos*"
+  "Buffer name when called apropos.")
+
 (defvar ivy-describe-modes--apropos-buffer nil
   "Record the apropos buffer.")
+
+(defvar ivy-describe-modes--cache nil
+  "Cache the mode list.")
 
 (defun ivy-describe-modes--clean-up ()
   "Do clean up after command."
@@ -50,37 +56,59 @@
     (kill-buffer ivy-describe-modes--apropos-buffer)
     (setq ivy-describe-modes--apropos-buffer nil)))
 
+(defun ivy-describe-modes--mute-apply (fnc &rest args)
+  "Execute FNC with ARGS without message."
+  (let ((message-log-max nil))
+    (with-temp-message (or (current-message) nil)
+      (let ((inhibit-message t))
+        (apply fnc args)))))
+
 (defun ivy-describe-modes--major-mode-list ()
   "Return a list of major modes."
-  (unless (ignore-errors (apropos-command "-mode$"))
-    (setq ivy-describe-modes--apropos-buffer (current-buffer))
-    
-    ))
-
-(defun ivy-describe-modes--minor-mode-list ()
-  "Return a list of minor modes."
-  (let ((final-lst '()))
-    (dolist (mode minor-mode-list)
-      (push (symbol-name mode) final-lst))
-    final-lst))
+  (let ((cmd-success t) (lst '()) (start -1) (end -1) (str nil)
+        (buf-str nil))
+    (save-window-excursion
+      (when (get-buffer ivy-describe-modes--apropos-name)
+        (kill-buffer ivy-describe-modes--apropos-name))
+      (ivy-describe-modes--mute-apply
+       (lambda ()
+         (setq cmd-success (ignore-errors (apropos-command "-mode$"))))))
+    (when cmd-success
+      (with-current-buffer ivy-describe-modes--apropos-name
+        (setq buf-str (buffer-string))
+        (setq ivy-describe-modes--apropos-buffer (current-buffer))
+        (goto-char (point-min))
+        (while (re-search-forward "-mode[ \t]*M-x" nil t)
+          (setq start (1- (line-beginning-position)))
+          (goto-char start)
+          (re-search-forward "[ \t]" nil t)
+          (setq end (- (point) 2))
+          (setq str (substring buf-str start end))
+          (push str lst)
+          (forward-line 1))))
+    lst))
 
 (defun ivy-describe-modes--candidates ()
   "List of canddidates, all modes."
-  (append (ivy-describe-modes--major-mode-list) (ivy-describe-modes--minor-mode-list)))
+  (unless ivy-describe-modes--cache
+    (setq ivy-describe-modes--cache (ivy-describe-modes--major-mode-list)))
+  ivy-describe-modes--cache)
 
 (defun ivy-describe-modes--do-action (cand)
-  "Do action after ivy done."
-  (describe-variable cand))
+  "Do action after ivy done with CAND."
+  (describe-variable (make-symbol cand)))
 
 ;;;###autoload
 (defun ivy-describe-modes ()
   "A convenient Ivy version of `describe-mode'."
   (interactive)
-  (ivy-read ivy-describe-modes--prompt
-            (ivy-describe-modes--candidates)
-            :require-match t
-            :action #'ivy-describe-modes--do-action)
-  (ivy-describe-modes--clean-up))
+  (unwind-protect
+      (ivy-read ivy-describe-modes--prompt
+                (ivy-describe-modes--candidates)
+                :require-match t
+                :sort t
+                :action #'ivy-describe-modes--do-action)
+    (ivy-describe-modes--clean-up)))
 
 
 (provide 'ivy-describe-modes)
